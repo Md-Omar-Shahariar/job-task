1ab9c489e8a17154204322b89c902e96d7c68abb
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-06-23 21:49:44 +0600

    sidebar

--- to-do-client/src/App.js
+++ to-do-client/src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home/Home";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Today from "./components/Home/Today";
+import Archive from "./components/Home/Archive";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}>
+          <Route path="today" element={<Today></Today>}></Route>
+          <Route path="archive" element={<Archive></Archive>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- to-do-client/src/components/Home/Home.js
+++ to-do-client/src/components/Home/Home.js
@@ -1,55 +1,87 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCoffee, faBars, faPlus } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div class="navbar bg-base-100 container mx-auto max-w-7xl flex justify-between">
-      <div class="">
-        <a class="font-serif normal-case text-[16px] font-[900]">TASKS</a>
-      </div>
-      <div class="form-control flex-1 mx-5">
-        <input
-          type="text"
-          placeholder="Search"
-          className=""
-          class="input input-bordered  w-full"
-        />
-      </div>
-      <div>
-        <button className="btn btn-primary hidden mr-5 md:flex items-center ">
-          <FontAwesomeIcon
-            className="text-[16px] mr-2"
-            icon={faPlus}
-          ></FontAwesomeIcon>
-          New Task
-        </button>
-      </div>
-      <div class="flex-none gap-2">
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-              <img src="https://api.lorem.space/image/face?hash=33791" />
-            </div>
-          </label>
-          <ul
-            tabindex="0"
-            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+    <div className="bg-base-100 container mx-auto max-w-7xl">
+      <div class="navbar  flex justify-between">
+        <div class="">
+          <label
+            for="my-drawer-2"
+            class="btn btn-ghost drawer-button mx-2 lg:hidden"
           >
+            <FontAwesomeIcon
+              className="text-[16px] mr-2"
+              icon={faBars}
+            ></FontAwesomeIcon>
+          </label>
+          <a class="font-serif normal-case text-[16px] font-[900]">TASKS</a>
+        </div>
+        <div class="form-control flex-1 mx-5">
+          <input
+            type="text"
+            placeholder="Search"
+            className=""
+            class="input input-bordered  w-full"
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary hidden mr-5 md:flex items-center ">
+            <FontAwesomeIcon
+              className="text-[16px] mr-2"
+              icon={faPlus}
+            ></FontAwesomeIcon>
+            New Task
+          </button>
+        </div>
+        <div class="flex-none gap-2">
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img src="https://api.lorem.space/image/face?hash=33791" />
+              </div>
+            </label>
+            <ul
+              tabindex="0"
+              class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <button className="btn btn-primary flex font-extrabold md:hidden items-center ">
+                  <FontAwesomeIcon
+                    className="text-[16px] mr-2"
+                    icon={faPlus}
+                  ></FontAwesomeIcon>
+                  New Task
+                </button>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center justify-center">
+          {/* <!-- Page content here --> */}
+          <Outlet></Outlet>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-2" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            {/* <!-- Sidebar content here --> */}
             <li>
-              <button className="btn btn-primary flex font-extrabold md:hidden items-center ">
-                <FontAwesomeIcon
-                  className="text-[16px] mr-2"
-                  icon={faPlus}
-                ></FontAwesomeIcon>
-                New Task
-              </button>
+              <Link to={"/today"}>Today</Link>
             </li>
             <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
+              <Link to={"/archive"}>Archive</Link>
             </li>
           </ul>
         </div>
